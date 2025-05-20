80782e4e1d904fd1abb9b30160bc133a0c6a8f9f
BloxerHD018 <bloxerhd018@gmail.com>
2025-05-20 22:37:18 +0100

    Note Context

--- src/commands/note.rs
+++ src/commands/note.rs
@@ -4,9 +4,11 @@ use serenity::all::{CommandInteraction, CommandOptionType, Context, CreateComman
 use sqlx::{Pool, Postgres};
 
 struct Note {
+    id: i64,
     name: String,
     note: String,
     owner: i64,
+    context: i64,
 }
 
 pub fn register() -> CreateCommand {
@@ -15,12 +17,17 @@ pub fn register() -> CreateCommand {
         .add_integration_type(InstallationContext::Guild)
         .add_integration_type(InstallationContext::User)
         .add_option(
-            CreateCommandOption::new(CommandOptionType::SubCommand, "new", "Creates a new note")
+            CreateCommandOption::new(CommandOptionType::SubCommand, "create", "Creates a new note")
                 .add_sub_option(
                     CreateCommandOption::new(CommandOptionType::String, "name", "The name of the note to create").required(true)
                 )
                 .add_sub_option(
                     CreateCommandOption::new(CommandOptionType::String, "note", "The note to be saved").required(true)
+                )
+                .add_sub_option(
+                    CreateCommandOption::new(CommandOptionType::String, "context", "Whether to find your user or server notes")
+                        .add_string_choice("Server", "server")
+                        .add_string_choice("User", "user")
                 )
         )
         .add_option(
@@ -29,13 +36,31 @@ pub fn register() -> CreateCommand {
                     CreateCommandOption::new(CommandOptionType::String, "name", "The name of the note to get").required(true)
                 )
                 .add_sub_option(
+                    CreateCommandOption::new(CommandOptionType::String, "context", "Whether to find your user or server notes")
+                        .add_string_choice("Server", "server")
+                        .add_string_choice("User", "user")
+                )
+                .add_sub_option(
                     CreateCommandOption::new(CommandOptionType::Boolean,"hidenote", "Whether the note should only show for you or for everyone")
                 )
         )
         .add_option(
-            CreateCommandOption::new(CommandOptionType::SubCommand, "remove", "Removes a note if you own it")
+            CreateCommandOption::new(CommandOptionType::SubCommand, "list", "Lists all of the tags you own")
+                .add_sub_option(
+                    CreateCommandOption::new(CommandOptionType::String, "context", "Whether to find your user or server notes")
+                        .add_string_choice("Server", "server")
+                        .add_string_choice("User", "user")
+                )
+        )
+        .add_option(
+            CreateCommandOption::new(CommandOptionType::SubCommand, "delete", "Removes a note if you own it")
                 .add_sub_option(
                     CreateCommandOption::new(CommandOptionType::String, "name", "The name of the note to delete").required(true)
+                )
+                .add_sub_option(
+                    CreateCommandOption::new(CommandOptionType::String, "context", "Whether to find your user or server notes")
+                        .add_string_choice("Server", "server")
+                        .add_string_choice("User", "user")
                 )
         )
 }
@@ -45,22 +70,67 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
         match &option.value {
             ResolvedValue::SubCommand(sub_options) => {
                 match &option.name[..] { // Sub command
-                    "new" => {
+                    "create" => {
                         let mut name = None;
                         let mut note = None;
+                        let mut context = None;
 
                         for option in sub_options { // Get values from interaction
                             match (&option.name[..], &option.value) {
                                 ("name", ResolvedValue::String(s)) => name = Some(s),
                                 ("note", ResolvedValue::String(s)) => note = Some(s),
+                                ("context", ResolvedValue::String(s)) => context = Some(s),
                                 _ => {}
+                            }
+                        };
+
+                        let query_context = match context {
+                            Some(&"server") => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
+                            }
+                            Some(&"user") => {
+                                interaction.user.id.get()
+                            }
+                            Some(&_a) => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
+                            }
+                            None => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
                             }
                         };
 
                         let exists = sqlx::query_as!( // Check if the note already exists
                             Note,
-                            "SELECT * FROM bot_hd_notes WHERE name = $1",
+                            "SELECT * FROM bot_hd_notes WHERE name = $1 AND context = $2",
                             name,
+                            query_context as i64,
                         )
                         .fetch_optional(&db)
                         .await;
@@ -68,22 +138,32 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
                         match exists {
                             Ok(Some(a)) => { // Note already exists - error
                                 let owner_id = UserId::new(a.owner as u64);
-                                let response_message = format!("ERROR: Note already exists\n`{:?}` (owned by {:?}):\n{:?}", name, owner_id, a.note);
+                                let owner = owner_id.to_user(&ctx.http).await.unwrap();
+                                let owner_name = owner.display_name();
+
+                                let context_text = if a.context as u64 == interaction.user.id.get() {
+                                    String::from("user")
+                                } else {
+                                    String::from("server")
+                                };
+
+                                let response_message = format!("ERROR: Note already exists\n`{}`'s {} note `{}`:\n{}", *name.unwrap(), owner_name, context_text, a.note);
                                 return CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content(response_message).ephemeral(true))
                             }
                             Ok(None) => { // Note doesn't exist - make it!
                                 let response = sqlx::query!(
-                                    "INSERT INTO bot_hd_notes (name, note, owner) VALUES ($1, $2, $3)",
+                                    "INSERT INTO bot_hd_notes (name, note, owner, context) VALUES ($1, $2, $3, $4)",
                                     name,
                                     note,
                                     interaction.user.id.get() as i64,
+                                    query_context as i64,
                                 )
                                 .execute(&db)
                                 .await;
 
                                 match response {
                                     Ok(_) => {
-                                        let response_message = format!("Successfully created note `{:?}`", name);
+                                        let response_message = format!("Successfully created note `{}`", *name.unwrap());
                                         return CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content(response_message).ephemeral(true))
                                     }
                                     Err(_) => {
@@ -99,19 +179,64 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
                     "get" => {
                         let mut name = None;
                         let mut hidenote = None;
+                        let mut context = None;
 
                         for option in sub_options { // Get values from interaction
                             match (&option.name[..], &option.value) {
                                 ("name", ResolvedValue::String(s)) => name = Some(s),
                                 ("hidenote", ResolvedValue::Boolean(b)) => hidenote = Some(b),
+                                ("context", ResolvedValue::String(s)) => context = Some(s),
                                 _ => {}
+                            }
+                        };
+
+                        let query_context = match context {
+                            Some(&"server") => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
+                            }
+                            Some(&"user") => {
+                                interaction.user.id.get()
+                            }
+                            Some(&_a) => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
+                            }
+                            None => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
                             }
                         };
 
                         let response = sqlx::query_as!(
                             Note,
-                            "SELECT * FROM bot_hd_notes WHERE name = $1",
-                            name
+                            "SELECT * FROM bot_hd_notes WHERE name = $1 AND context = $2",
+                            name,
+                            query_context as i64,
                         )
                         .fetch_optional(&db)
                         .await;
@@ -122,9 +247,16 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
                                 let owner = owner_id.to_user(&ctx.http).await.unwrap();
                                 let owner_name = owner.display_name();
 
+                                let context_text = if a.context as u64 == interaction.user.id.get() {
+                                    String::from("user")
+                                } else {
+                                    String::from("server")
+                                };
+
                                 let response_text = format!(
-                                    "`{}`'s note `{}`:\n{}",
+                                    "`{}`'s {} note `{}`:\n{}",
                                     owner_name,
+                                    context_text,
                                     a.name,
                                     a.note
                                 );
@@ -147,22 +279,65 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
                             }
                         }
                     }
-                    "remove" => {
-                        let name = sub_options
-                            .iter()
-                            .find(|opt| opt.name == "name")
-                            .and_then(|opt| {
-                                if let ResolvedValue::String(s) = &opt.value {
-                                    Some(s)
-                                } else {
-                                    None
-                                }
-                            });
+                    "delete" => {
+                        let mut name = None;
+                        let mut context = None;
+
+                        for option in sub_options { // Get values from interaction
+                            match (&option.name[..], &option.value) {
+                                ("name", ResolvedValue::String(s)) => name = Some(s),
+                                ("context", ResolvedValue::String(s)) => context = Some(s),
+                                _ => {}
+                            }
+                        };
                         
+                        let query_context = match context {
+                            Some(&"server") => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
+                            }
+                            Some(&"user") => {
+                                interaction.user.id.get()
+                            }
+                            Some(&_a) => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
+                            }
+                            None => {
+                                let guild_id = interaction.guild_id;
+
+                                match guild_id {
+                                    Some(gid) => {
+                                        gid.get()
+                                    }
+                                    None => {
+                                        interaction.user.id.get()
+                                    }
+                                }
+                            }
+                        };
+
                         let exists = sqlx::query_as!(
                             Note,
-                            "SELECT * FROM bot_hd_notes WHERE name = $1",
-                            name
+                            "SELECT * FROM bot_hd_notes WHERE name = $1 AND context = $2",
+                            name,
+                            query_context as i64,
                         )
                         .fetch_optional(&db)
                         .await;
@@ -171,8 +346,9 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
                             Ok(Some(a)) => { // Note exists - Verify if the user owns it
                                 if interaction.user.id.get() == a.owner as u64 {
                                     let response = sqlx::query!(
-                                        "DELETE FROM bot_hd_notes WHERE name = $1",
-                                        name
+                                        "DELETE FROM bot_hd_notes WHERE name = $1 AND context = $2",
+                                        name,
+                                        query_context as i64
                                     )
                                     .execute(&db)
                                     .await;
@@ -199,7 +375,7 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
                             }
                         }
                     }
-                    _ => {return CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content("ERROR: This subcommand is not implemented"))}
+                    _ => {return CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content("ERROR: This subcommand is not implemented").ephemeral(true))}
                 }
             }
             _ => {
@@ -208,5 +384,5 @@ pub async fn execute<'a>(options: &[ResolvedOption<'a>], interaction: CommandInt
         }
     };
 
-    CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content("ERROR: No Options Found"))
+    CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content("ERROR: No Options Found").ephemeral(true))
 }
