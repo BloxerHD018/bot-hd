4510afce9e85c3bb39ffc6d8f01a9eccf9226eb3
BloxerHD018 <bloxerhd018@gmail.com>
2025-05-21 22:56:33 +0100

    Made note a global command

--- src/events/ready.rs
+++ src/events/ready.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use serenity::{
-    all::GuildId, http, model::gateway::Ready, prelude::*
+    all::{GuildId, Command}, http, model::gateway::Ready, prelude::*
 };
 
 use crate::commands;
@@ -9,6 +9,14 @@ use crate::commands;
 pub async fn handle(ctx: Context, ready: Ready) {
     println!("Connected to Discord as {}", ready.user.name);
 
+    // Delete global commands -- TESTING ONLY DONT LEAVE ON
+    /*let commands = http::Http::get_global_commands(&ctx.http).await.unwrap();
+    for command in commands {
+        let _ = http::Http::delete_global_command(&ctx.http, command.id).await;
+        println!("Deleted {} global command", command.name);
+    }*/
+
+    // Register Guild Commands
     let guild_id = GuildId::new(
         env::var("TEST_GUILD_ID")
             .expect("TEST_GUILD_ID must be set when using guild commands")
@@ -16,17 +24,9 @@ pub async fn handle(ctx: Context, ready: Ready) {
             .expect("TEST_GUILD_ID must be an integer"),
     );
 
-    // Delete global commands -- TESTING ONLY DONT LEAVE ON
-    let commands = http::Http::get_global_commands(&ctx.http).await.unwrap();
-    for command in commands {
-        let _ = http::Http::delete_global_command(&ctx.http, command.id).await;
-        println!("Deleted {} global command", command.name);
-    }
-
     let guild_commands = guild_id
         .set_commands(&ctx.http, vec![
             commands::ping::register(),
-            commands::note::register(),
         ]).await;
 
     if let Err(e) = guild_commands {
@@ -34,5 +34,5 @@ pub async fn handle(ctx: Context, ready: Ready) {
     };
 
     // Register Global Commands
-    //let _ = Command::create_global_command(&ctx.http, commands::ping::register()).await;
+    let _ = Command::create_global_command(&ctx.http, commands::note::register()).await;
 }
